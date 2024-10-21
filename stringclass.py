a3694ed3fa6f033a22ae1d8deed3f5e051b4ce4a
Mahfuz1203 <Amusamafus82@gmail.com>
2024-10-22 02:20:43 +0900

    Added value to stringclass

---
+++ stringclass.py
@@ -0,0 +1,25 @@
+full_name = '                 AMUSA MAFUS'
+age = 62
+# print(full_name.strip())
+# print(len(full_name))
+# print(full_name)
+# print(len(full_name.strip()))
+print('hello my name is ' + full_name +' and i am '+ str(age))
+
+
+# print(f'Hello my name is {full_name}.and i am {age}')
+
+# course = 'python for level'
+# courselevel = input('put your python level here?')
+# print(courselevel)
+# print(course.replace('level', courselevel))
+# fullname = input('what is your name?')
+# Age = input('what is your age?')
+# print('my name is' + fullname +' i am '+ str(age))
+# print('i am'+ str(age))
+# print(course.replace('beginner', 'intermediate'))
+
+# print(course.upper())
+# print(course.lower())
+
+
